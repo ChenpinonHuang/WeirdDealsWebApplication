7634bc9510a565154c7527acda5610689a819e10
ChenpinonHuang <52473995+ChenpinonHuang@users.noreply.github.com>
2019-12-16 18:09:31 -0500

    Add files via upload

--- application.js
+++ application.js
@@ -1,8 +1,10 @@
 //Create variables that store a reference to section elements
+let header = document.querySelector('header');
 let section = document.querySelector('section');
 
 //Create a variable to store request URL
-let requestURL = "https://github.com/ChenpinonHuang/WeirdDealsWebApplication/blob/master/weirdDeals.json";
+let requestURL = "https://chenpinonhuang.github.io/WeirdDealsWebApplication/weirdDeals.json";
+//let requestURL = "weirdDeals.json";
 
 // Create a new XHR object
 let request = new XMLHttpRequest();
@@ -24,13 +26,29 @@ request.onload = function() {
   showDeals(weirdDeal);
 }
 
+// Set up populateHeader function to fill in the header function
+function populateHeader(jsonObj) {
+
+  // grab the company name from JSON object and then create a new element, adding text and appending to the header
+
+  let headerH3 = document.createElement('h3');
+  headerH3.textContent = jsonObj['siteName'];
+  header.appendChild(headerH3);
+
+  //grab the company info and established date and add a new paragraph to your HTML that displays this info
+
+  let headerPara = document.createElement('p');
+  headerPara.textContent = 'Head Office: ' + jsonObj['headOffice'] + ' , Established:  ' + jsonObj['established'];
+  header.appendChild(headerPara);
+}
+
 // define the show function to show the weird deals
 function showDeals(jsonObj) {
 
   //bind top five weird deal object to a variables
   let proudct = jsonObj['proudct'];
 
-  for (let i = 0; i < topFive.length; i++) {
+  for (let i = 0; i < proudct.length; i++) {
     //create a few different elements
     let article = document.createElement('article');
     let h2 = document.createElement('h2');
@@ -47,6 +65,10 @@ function showDeals(jsonObj) {
     description.textContent = 'description: ' + proudct[i].description;
     features.textContent = 'description: ' + proudct[i].description;
 
+    let LatNum = proudct[i].Latitude;
+    let LonNum = proudct[i].Longitude;
+    initMap(LatNum, LonNum);
+
     // Append each element to article, then append article to section
 
     article.appendChild(img);
@@ -58,4 +80,15 @@ function showDeals(jsonObj) {
 
   }
 
+}
+
+function initMap(LatNum, LonNum) {
+  let proudct = jsonObj['proudct'];
+  // The location of Uluru
+  var uluru = {lat: LatNum, lng: LonNum};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 8, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
--- googleMap.js
+++ googleMap.js
@@ -1,10 +1,1 @@
 // Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 44.389355, lng: -79.690331};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-    document.getElementById('map'), {zoom: 8, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
